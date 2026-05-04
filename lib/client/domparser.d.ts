78648adb19531628801d6a61a21daa2c8ecfd84e
Remigijus Kiminas <remdex@gmail.com>
2026-05-04 12:59:44 +0000

    Build files

---
+++ lib/client/domparser.d.ts
@@ -0,0 +1,10 @@
+import type { TrustedTypePolicyLike } from '../types';
+/**
+ * Parses HTML string to DOM nodes.
+ *
+ * @param html - HTML markup.
+ * @param trustedTypePolicy - Trusted Types policy.
+ * @returns - DOM nodes.
+ */
+export default function domparser(html: string, trustedTypePolicy?: TrustedTypePolicyLike): NodeList;
+//# sourceMappingURL=domparser.d.ts.map
